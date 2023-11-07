9003a27cc0f57185ae9b5adcbf0db05ff58c4a2c
marcel <Marcel@mantzinos.de>
2023-11-07 17:42:24 +0100

    Bilder Funktion Funktioniert

--- src/components/bilder-txt.vue
+++ src/components/bilder-txt.vue
@@ -1,40 +1,32 @@
 <template>
-  <button @click="nextesbild">next png</button>
-  <p>{{ bildaktiv }}</p>
-  <img src="../assets/blender_logo.jpg" v-if="(bildaktiv = 'blender')" />
+  <button @click="weiter">{{counter}}</button>
+  <img src="../assets/blender_logo.jpg" v-if="(counter == 0)" />
 
-  <img src="../assets/blender_logo.jpg" v-else-if="(bildaktiv = 'fusion')" />
-  <img src="" v-else-if="(bildgerade = 'cake')" />
-  <img src="../assets/logo.png" v-else-if="(bildaktiv = 'cake')" />
-
-  <p>{{ bildaktiv }}</p>
+  <img src="../assets/blender_logo.jpg" v-else-if="(counter == 1)" />
+  <img src="" v-else-if="(counter == 2)" />
+  <img src="../assets/logo.png" v-else-if="(counter == 3)" />
 </template>
 
 <script>
+import {ref} from 'vue'
 export default {
   /*code from w3 scholl vue / vue Animations / transition Between elements */
   data() {
     return {
-      bildaktiv: "blender", //das bild welches in der webseite aktiv ist
-      bilderarray: ["blender", "fusion", "cake", "fish", "rice"], //array mitt allen namen der zirkulirenden hintergrund billdern
-      bildgerade: 1, // startwert bei welchem bild angefangen wird
+      weiter:weiter,
+      counter:counter
     };
   },
-
-  methods: {
-    // funktion zum wechseln der bilder
-    nextesbild() {
-       console.log('hallo');
-
-      this.bildgerade++;
-
-      if (this.bildgerade >= this.bilderarray.length) {
-        this.bildgerade = 1;
-      }
-      this.bildaktiv = this.bilderarray[this.bildgerade];
-    },
-  },
 };
+const counter = ref(0)
+const end = ref(4)
+ function weiter(){
+  counter.value++
+  if (counter.value >= end.value){
+    counter.value = 0
+  }
+}
+
 </script>
 
 <style scoped>
