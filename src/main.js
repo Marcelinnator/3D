535208eb2410fe49d3dce43208d7f99e22f8dc94
marcel <Marcel@mantzinos.de>
2023-11-10 20:41:24 +0100

    Router

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from 'vue'
+import router from './router'
+const app = createApp(App)
 //inport Anzeigeinhalt from '/.Anzeigeinhalt.vue'
-createApp(App).mount('#app')
+app.use(router)
+app.mount('#app')
