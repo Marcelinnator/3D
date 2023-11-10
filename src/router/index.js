407abf7bb413131c0b89a22b9b570b67520b90ac
marcel <Marcel@mantzinos.de>
2023-11-10 21:42:40 +0100

    fixed bug with empty page

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,11 @@
 import { createRouter, createWebHashHistory } from "vue-router";
+
 const router = createRouter({
     history: createWebHashHistory(),
-    routes: []
+    routes: [
+
+    ]
 })
+
 
 export default router
